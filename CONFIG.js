e62e039d2335fb6e2099931daaa795622a823463
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2024-03-08 13:52:50 +0700

    88

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Đéo",
   titleModar: "Chúc pé iu 8/3 zui zẻ hehe!",
   descriptionModar:
-    "Chúc em pé 8/3 zui zẻ. Chúc pé iu ngày càng xink đẹp zui zẻ iu đời zới bớt tiu cực dùm tui </br> Chúc em mãi là của tui </br> Chúc em mãi là của tui </br> Chúc em mãi là của tui </br> Iu emmmmmmmmmmmmmmmmmmmmmmmmm <3 </br> From Thenk En with luv <3",
+    "Chúc em pé 8/3 zui zẻ. Chúc pé iu ngày càng xink đẹp zui zẻ iu đời zới bớt tiu cực dùm tui </br> Chúc em mãi là của tui</br> Chúc em mãi là của tui </br> Chúc em mãi là của tui </br> Iu emmmmmmmmmmmmmmmmmmmmmmmmm <3 </br> From Thenk En with luv <3",
 };
